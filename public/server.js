e48257c3e7588307c8c7fb42561611552db87c4b
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-10-10 17:32:59 -0700

    got wildcard route to return index.html

--- public/server.js
+++ public/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const noteData = require("./../db/db.json")
+
 
 const app = express();
 
@@ -13,6 +15,23 @@ app.get('/' ,(req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/notes.html'))
+});
+
+app.get('/notes/:id', (req, res) => {
+    console.log(req.params)
+    const requestedNote = req.params.id;
+    for (let i = 0; i < noteData.length; i++){
+        if(requestedNote === noteData[i].id){
+        return res.json(noteData[i]);
+    }
+}
+return res.json('No such note found');
+});
+
+app.get('/db', (req, res) => res.json(noteData));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
 });
 
 app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
